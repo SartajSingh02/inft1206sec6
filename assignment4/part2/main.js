c4bcb2f82b0c71a15d5e05860ec32410c5721da7
SartajSingh02 <sartajsingh5@dcmail.ca>
2024-07-30 14:40:38 -0400

    reupdate main.js

--- assignment4/part2/main.js
+++ assignment4/part2/main.js
@@ -21,3 +21,11 @@ btn.addEventListener('click', () => {
     if (btnClass === 'dark') {
       btn.setAttribute('class','light');
       btn.textContent = 'Lighten';
+      overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+    } else {
+      btn.setAttribute('class','dark');
+      btn.textContent = 'Darken';
+      overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+    }
+  });
+  
